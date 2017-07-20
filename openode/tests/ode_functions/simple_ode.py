6748083f075a1eb7caccdfdbd7f6d4c4ccd400dd
John Hwang <hwangjt@Johns-MacBook-Pro-2015.local>
2017-07-20 16:20:02 -0400

    Added BDF4

--- openode/tests/ode_functions/simple_ode.py
+++ openode/tests/ode_functions/simple_ode.py
@@ -58,6 +58,34 @@ class LinearODE(ExplicitComponent):
             * (-(2 * np.pi) ** 2 * np.sin(two_pi_t) - 2 * np.pi * np.cos(two_pi_t))
 
 
+class SimpleODE(ExplicitComponent):
+
+    def initialize(self):
+        self.metadata.declare('num', default=1, type_=int)
+
+    def setup(self):
+        num = self.metadata['num']
+
+        self.add_input('y', shape=(num, 1))
+        self.add_input('t', shape=(num, 1))
+        self.add_output('dy_dt', shape=(num, 1))
+
+        self.declare_partials('dy_dt', 'y', val=np.eye(num))
+
+        self.eye = np.eye(num)
+
+    def compute(self, inputs, outputs):
+        outputs['dy_dt'] = inputs['y']
+
+
+class SimpleODEFunction(ODEFunction):
+
+    def initialize(self):
+        self.set_system(SimpleODE)
+        self.declare_state('y', rate_target='dy_dt', state_targets='y')
+        self.declare_time('t')
+
+
 class LinearODEFunction(ODEFunction):
 
     def initialize(self):
