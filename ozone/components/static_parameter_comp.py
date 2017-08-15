d2bf04e8874ca9cbf7a34eb1f2d0a8c747a156e4
John Hwang <hwangjt@Johns-MacBook-Pro-2015.local>
2017-08-14 22:18:42 -0400

    added static param comp

---
+++ ozone/components/static_parameter_comp.py
@@ -0,0 +1,37 @@
+import numpy as np
+from six import iteritems
+import scipy.sparse
+
+from openmdao.api import ExplicitComponent
+
+from ozone.utils.var_names import get_name
+from ozone.utils.units import get_rate_units
+from ozone.utils.sparse_linear_spline import get_sparse_linear_spline
+
+
+class StaticParameterComp(ExplicitComponent):
+
+    def initialize(self):
+        self.metadata.declare('static_parameters', type_=dict, required=True)
+
+    def setup(self):
+        for parameter_name, parameter in iteritems(self.metadata['static_parameters']):
+            size = np.prod(parameter['shape'])
+            shape = parameter['shape']
+
+            in_name = get_name('in', parameter_name)
+            out_name = get_name('out', parameter_name)
+
+            self.add_input(in_name, shape=shape, units=parameter['units'])
+            self.add_output(out_name, shape=shape, units=parameter['units'])
+
+            ones = np.ones(size)
+            arange = np.arange(size)
+            self.declare_partials(out_name, in_name, val=ones, rows=arange, cols=arange)
+
+    def compute(self, inputs, outputs):
+        for parameter_name, parameter in iteritems(self.metadata['static_parameters']):
+            in_name = get_name('in', parameter_name)
+            out_name = get_name('out', parameter_name)
+
+            outputs[out_name] = inputs[in_name]
